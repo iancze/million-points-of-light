a6d9085ffea0a38bc54c3cc28c47968108027e47
Ian Czekala <iancze@gmail.com>
2019-02-23 18:16:00 -0800

    a more full-fledged gridding routine.

--- gridding.py
+++ gridding.py
@@ -131,98 +131,108 @@ def gcffun(eta):
 
 # know that the v stride is the number of u points.
 
-def calc_row(u_data, v_data, u_model, v_model):
+def calc_matrices(data_points, u_model, v_model):
     '''
-    Calcuate the 36 coefficients for the row.
+    Calcuate the real and imaginary interpolation matrices in one pass.
 
     Args:
-        u_data: the u point (in klambda)
-        v_data: the v point (in klambda)
-        u_model: the array of u points delivered by the rfft
-        v_model: the array of v points delivered by the rfft
+        data_points: the pairs of u,v points in the dataset (in klambda)
+        u_model: the u axis delivered by the rfft (unflattened). Assuming this is the RFFT axis.
+        v_model: the v axis delivered by the rfft (unflattened). Assuming this is the FFT axis.
+
+    Start with an image packed like Img[j, i]. i is the alpha index and j is the delta index.
+    Then the RFFT output will have RFFT[j, i]. i is the u index and j is the v index.
+
+    see also `Model.for` routine in MIRIAD source code.
+    Uses spheroidal wave functions to interpolate a model to a (u,v) coordinate.
+    u,v are in [kλ]
+
     '''
 
+    # assert that the maximum baseline is contained within the model grid.
+    # assert that the image-plane pixels are small enough.
 
-# called ModGrid in gridding.c (KR code) and in Model.for (MIRIAD)
-# Uses spheroidal wave functions to interpolate a model to a (u,v) coordinate.
-# u,v are in [kλ]
+    # number of visibility points in the dataset
+    N_vis = len(data_points)
 
-"
-    interpolate_uv(u::Float64, v::Float64, vis::FullModelVis)
-Interpolates a dense grid of visibilities (e.g., from FFT of an image) to a specfic (u,v) point using spheroidal functions in a band-limited manner designed to reduce aliasing.
-"
-function interpolate_uv(u::Float64, v::Float64, vis::FullModelVis)
+    # calculate the stride needed to advance one v point when the array is flattened
+    # (the length of the u row).
+    vstride = len(u_model)
+    Npix = len(v_model)
 
-    # Note that vis.uu goes from positive to negative (East-West)
-    # and vis.vv goes from negative to positive (North-South)
+    assert Npix % 2 == 0, "Npix (delta-direction) is not even."
 
-    # 1. Find the nearest gridpoint in the FFT'd image.
-    iu0 = argmin(abs.(u .- vis.uu))
-    iv0 = argmin(abs.(v .- vis.vv))
 
-    # now find the relative distance from (u,v) to this nearest grid point (not absolute)
-    u0 = u - vis.uu[iu0]
-    v0 = v - vis.vv[iv0]
+    # initialize two sparse matrices. For now, just call them zeros.
+    C_real = np.zeros((N_vis, (Npix * vstride)), dtype=np.float64)
+    C_imag = np.zeros((N_vis, (Npix * vstride)), dtype=np.float64)
 
-    # determine the uu and vv distance for 3 grid points (could be later taken out)
-    du = abs.(vis.uu[4] - vis.uu[1])
-    dv = abs.(vis.vv[4] - vis.vv[1])
+    # since we will probably have the case that len(data_points) > (len(u_model) * len(v_model)),
+    # it is more efficient to use a compressed sparse column (row) matrix
+    # according to Theano,
+    # If shape[0] > shape[1], use csc format. Otherwise, use csr.
 
-    # 2. Calculate the appropriate u and v indexes for the 6 nearest pixels
-    # (3 on either side)
+    # determine model grid spacing
+    du = np.abs(u_model[1] - u_model[0])
+    dv = np.abs(v_model[1] - v_model[0])
 
-    # Are u0 and v0 to the left or the right of the index?
-    # we want to index three to the left, three to the right
+    # for each data_point within the grid, calculate the row and insert it into the matrix
+    for row_index, (u, v) in enumerate(data_points):
 
-    # First check that our (u,v) point still exists within the appropriate margins of the
-    # dense visibility array
-    # This is to make sure that at least three grid points exist in all directions
-    # If this fails, this means that the synthesized image is too large compared to the sampled visibilities (meaning that the dense FFT grid is too small).
-    # The max u,v sampled is 2/dRA or 2/dDec. This means that if dRA or dDEC is too large, then this
-    # will fail
-    lenu = length(vis.uu)
-    lenv = length(vis.vv)
-    @assert iu0 >= 4
-    @assert iv0 >= 4
-    @assert lenu - iu0 >= 4
-    @assert lenv - iv0 >= 4
+        # assemble a list of l indices into the flattened RFFT output
+        # see if we have an edge case where we  overlap with u=0 or v=0 or not.
+        if (np.abs(u) > 3 * du) and (np.abs(v) > 3 * dv):
+            # normal interpolation can proceed
 
-    if u0 >= 0.0
-        # To the right of the index
-        uind = iu0-2:iu0+3
-    else
-        # To the left of the index
-        uind = iu0-3:iu0+2
-    end
+            # find the nearest points in the array
+            i0 = np.searchsorted(u_model)
 
-    if v0 >= 0.0
-        # To the right of the index
-        vind = iv0-2:iv0+3
-    else
-        # To the left of the index
-        vind = iv0-3:iv0+2
-    end
+            if (v > 0):
+                j0 = np.searchsorted(v_model[Npix/2], u) # only search the positive frequencies
+            else:
+                #(v < 0)
+                j0 = np.searchsorted(v_model[Npix/2:], v) + Npix/2 # only search the negative frequencies
 
-    etau = (vis.uu[uind] .- u)/du
-    etav = (vis.vv[vind] .- v)/dv
-    VV = vis.VV[vind, uind] # Array is packed like the image
+            is = np.arange(i0 - 3, i0 + 3)# 6 points [i0-3,i0-2,i0-1,i0,i0+1,i0+2]
+            js = np.arange(j0 - 3, j0 + 3)# 6 points [j0-3,j0-2,j0-1,j0,j0+1,j0+2]
 
-    # 3. Calculate the weights corresponding to these 6 nearest pixels (gcffun)
-    uw = gcffun(etau)
-    vw = gcffun(etav)
+            ls = np.array([i + j * vstride for i in is for j in js]) # list of 36 l indices
 
-    # 4. Normalization such that it has an area of 1. Divide by w later.
-    w = sum(uw) * sum(vw)
+            # assemble the real interpolation row
 
-    # 5. Loop over all 36 grid indices and sum to find the interpolation.
-    cumulative::ComplexF64 = 0.0 + 0.0im
-    for i=1:6
-        for j=1:6
-            cumulative += uw[i] * vw[j] * VV[j,i] # Array is packed like the image
-        end
-    end
+            # calculate the u and v distances from the (u, v) datapoint to each of the l indices as a function of
+            # eta in the domain 0 - 1
+            u_etas = (u - u_model[is]) / du
+            v_etas = (v - v_model[js]) / dv
 
-    cumulative = cumulative/w
+            # evaluate the spheroid here
+            uw = gcffun(u_etas)
+            vw = gcffun(v_etas)
 
-    return cumulative
-end
+            # Normalization such that it has an area of 1. Divide by w later.
+            w = sum(uw) * sum(vw)
+
+            # actual weight at a point is uw[i] * vw[j] / w
+            # arrange the uw and vw weights in the same order as ls
+            weight_row = np.array([uw[i] * vw[j]]) / w
+
+            # insert this into the C matrix at the corresponding locations (the ls)
+            C_real[row_index,ls] = weight_row
+
+            # assemble the complex interpolation matrix
+            if (u > 0):
+                # do the normal thing for imag
+                C_imag[row_index,ls] = weight_row
+            else:
+                # u < 0
+                # put in the complex conjugate, which is -
+                C_imag[row_index,ls] = -weight_row
+
+        else:
+            # consider all of the tricky edge cases
+            # just a flag for now to emphasize that we haven't handled these cases
+            C_real[row_index,:] = np.nan
+            C_imag[row_index,:] = np.nan
+
+
+        return C_real, C_imag
