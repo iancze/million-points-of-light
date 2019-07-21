c63ad9cc781ca52b5538c8679699b86fbac4f327
Ian Czekala <iancze@gmail.com>
2019-07-22 07:11:56 +1000

    reforatted style w/ black.

--- gridding.py
+++ gridding.py
@@ -5,63 +5,77 @@ from scipy.sparse import csc_matrix
 
 # fill the sparse interpolation matrix.
 
+
 def horner(x, a):
-    '''
+    """
     Use Horner's method to compute and return the polynomial
         a[0] + a[1] x^1 + a[2] x^2 + ... + a[n-1] x^(n-1)
     evaluated at x.
 
     from https://introcs.cs.princeton.edu/python/21function/horner.py.html
-    '''
+    """
     result = 0
-    for i in range(len(a)-1, -1, -1):
+    for i in range(len(a) - 1, -1, -1):
         result = a[i] + (x * result)
     return result
 
+
 @np.vectorize
 def spheroid(eta):
-    '''
+    """
         `spheroid` function which assumes ``\\alpha`` = 1.0, ``m=6``,  built for speed."
 
         Args:
             eta (float) : the value between [0, 1]
-    '''
+    """
 
     # Since the function is symmetric, overwrite eta
     eta = np.abs(eta)
 
-    if (eta <= 0.75):
-        nn = eta**2 - 0.75**2
+    if eta <= 0.75:
+        nn = eta ** 2 - 0.75 ** 2
 
-        return horner(nn, np.array([8.203343E-2, -3.644705E-1, 6.278660E-1, -5.335581E-1, 2.312756E-1])) / horner(nn, np.array([1., 8.212018E-1, 2.078043E-1]))
+        return horner(
+            nn,
+            np.array(
+                [8.203343e-2, -3.644705e-1, 6.278660e-1, -5.335581e-1, 2.312756e-1]
+            ),
+        ) / horner(nn, np.array([1.0, 8.212018e-1, 2.078043e-1]))
 
-    elif (eta <= 1.0):
-        nn = eta**2 - 1.0
+    elif eta <= 1.0:
+        nn = eta ** 2 - 1.0
 
-        return horner(nn, np.array([4.028559E-3, -3.697768E-2, 1.021332E-1, -1.201436E-1, 6.412774E-2])) / horner(nn, np.array([1., 9.599102E-1, 2.918724E-1]))
+        return horner(
+            nn,
+            np.array(
+                [4.028559e-3, -3.697768e-2, 1.021332e-1, -1.201436e-1, 6.412774e-2]
+            ),
+        ) / horner(nn, np.array([1.0, 9.599102e-1, 2.918724e-1]))
 
-    elif (eta <= 1.0 + 1e-7):
+    elif eta <= 1.0 + 1e-7:
         # case to allow some floating point error
         return 0.0
 
     else:
         # Now you're really outside of the bounds
-        print("The spheroid is only defined on the domain -1.0 <= eta <= 1.0. (modulo machine precision.)")
+        print(
+            "The spheroid is only defined on the domain -1.0 <= eta <= 1.0. (modulo machine precision.)"
+        )
         raise ValueError
 
 
 def corrfun(eta):
-    '''
+    """
     Gridding *correction* function, but able to be passed either floating point numbers or vectors of `Float64`."
 
     Args:
         eta (float): the value in [0, 1]
-    '''
+    """
     return spheroid(eta)
 
 
 def corrfun_mat(alphas, deltas):
-    '''
+    """
     Calculate the pre-multiply correction function to the image.
     Return as a 2D array.
 
@@ -69,7 +83,7 @@ def corrfun_mat(alphas, deltas):
         alphas (1D array): RA list (pre-fftshifted)
         deltas (2D array): DEC list (pre-fftshifted)
 
-    '''
+    """
 
     ny = len(deltas)
     nx = len(alphas)
@@ -79,13 +93,13 @@ def corrfun_mat(alphas, deltas):
     # The size of one half-of the image.
     # sometimes ra and dec will be symmetric about 0, othertimes they won't
     # so this is a more robust way to determine image half-size
-    maxra = np.abs(alphas[2] - alphas[1]) * nx/2
-    maxdec = np.abs(deltas[2] - deltas[1]) * ny/2
+    maxra = np.abs(alphas[2] - alphas[1]) * nx / 2
+    maxdec = np.abs(deltas[2] - deltas[1]) * ny / 2
 
     for i in range(nx):
         for j in range(ny):
-            etax = (alphas[i])/maxra
-            etay = (deltas[j])/maxdec
+            etax = (alphas[i]) / maxra
+            etay = (deltas[j]) / maxdec
 
             if (np.abs(etax) > 1.0) or (np.abs(etay) > 1.0):
                 # We would be querying outside the shifted image
@@ -96,20 +110,21 @@ def corrfun_mat(alphas, deltas):
 
     return mat
 
+
 def gcffun(eta):
-    '''
+    """
     The gridding *convolution* function, used to do the convolution and interpolation of the visibilities in
     the Fourier domain. This is also the Fourier transform of `corrfun`.
 
     Args:
         eta (float): in the domain of [0,1]
-    '''
+    """
 
-    return np.abs(1.0 - eta**2) * spheroid(eta)
+    return np.abs(1.0 - eta ** 2) * spheroid(eta)
 
 
 def calc_matrices(data_points, u_model, v_model):
-    '''
+    """
     Calcuate the real and imaginary interpolation matrices in one pass.
 
     Args:
@@ -131,10 +146,10 @@ def calc_matrices(data_points, u_model, v_model):
     v freqs stored like this
     f = [0, 1, ...,   n/2-1,     -n/2, ..., -1] / (d*n)   if n is even
 
-    '''
+    """
 
-    #TODO: assert that the maximum baseline is contained within the model grid.
-    #TODO: assert that the image-plane pixels are small enough.
+    # TODO: assert that the maximum baseline is contained within the model grid.
+    # TODO: assert that the image-plane pixels are small enough.
 
     # number of visibility points in the dataset
     N_vis = len(data_points)
@@ -151,20 +166,19 @@ def calc_matrices(data_points, u_model, v_model):
     du = np.abs(u_model[1] - u_model[0])
     dv = np.abs(v_model[1] - v_model[0])
 
-
     # for each data_point within the grid, calculate the row and insert it into the matrix
     for row_index, (u, v) in enumerate(data_points):
 
         # assuming the grid stretched for -/+ values easily
-        i0 = np.int(np.ceil(u/du))
-        j0 = np.int(np.ceil(v/dv))
+        i0 = np.int(np.ceil(u / du))
+        j0 = np.int(np.ceil(v / dv))
 
         i_indices = np.arange(i0 - 3, i0 + 3)
         j_indices = np.arange(j0 - 3, j0 + 3)
 
         # calculate the etas and weights
-        u_etas = (u/du - i_indices) / 3
-        v_etas = (v/dv - j_indices) / 3
+        u_etas = (u / du - i_indices) / 3
+        v_etas = (v / dv - j_indices) / 3
 
         uw = gcffun(u_etas)
         vw = gcffun(v_etas)
@@ -187,9 +201,9 @@ def calc_matrices(data_points, u_model, v_model):
                     j_index = j_indices[j]
                     imag_prefactor = 1.0
 
-                else: # i_index < 0:
+                else:  # i_index < 0:
                     # map negative i index to positive i_index
-                    i_index = - i_index
+                    i_index = -i_index
 
                     # map j index to opposite sign
                     j_index = -j_indices[j]
@@ -206,7 +220,9 @@ def calc_matrices(data_points, u_model, v_model):
                 weights_imag[k] = imag_prefactor * uw[i] * vw[j] / w
 
         # TODO: at the end, decide whether there is overlap and consolidate
-        l_sorted, unique_indices, unique_inverse, unique_counts = np.unique(l_indices, return_index=True, return_inverse=True, return_counts=True)
+        l_sorted, unique_indices, unique_inverse, unique_counts = np.unique(
+            l_indices, return_index=True, return_inverse=True, return_counts=True
+        )
         if len(unique_indices) < 36:
             # Some indices are querying the same point in the RFFT grid, and their weights need to be
             # consolidated
@@ -216,7 +232,7 @@ def calc_matrices(data_points, u_model, v_model):
             condensed_weights_imag = np.zeros(N_unique)
 
             # find where unique_counts > 1
-            ind_multiple = (unique_counts > 1)
+            ind_multiple = unique_counts > 1
 
             # take the single weights from the array
             ind_single = ~ind_multiple
@@ -225,8 +241,12 @@ def calc_matrices(data_points, u_model, v_model):
             # unique_indices[ind_single]
 
             # get the weights of the values that only occur once
-            condensed_weights_real[ind_single] = weights_real[unique_indices[ind_single]]
-            condensed_weights_imag[ind_single] = weights_imag[unique_indices[ind_single]]
+            condensed_weights_real[ind_single] = weights_real[
+                unique_indices[ind_single]
+            ]
+            condensed_weights_imag[ind_single] = weights_imag[
+                unique_indices[ind_single]
+            ]
 
             # the indices that occur multiple times
             # l_sorted[ind_multiple]
@@ -247,10 +267,8 @@ def calc_matrices(data_points, u_model, v_model):
             weights_real = condensed_weights_real
             weights_imag = condensed_weights_imag
 
-
-        C_real[row_index,l_indices] = weights_real
-        C_imag[row_index,l_indices] = weights_imag
-
+        C_real[row_index, l_indices] = weights_real
+        C_imag[row_index, l_indices] = weights_imag
 
     C_real_sparse = csc_matrix(C_real)
     C_imag_sparse = csc_matrix(C_imag)
